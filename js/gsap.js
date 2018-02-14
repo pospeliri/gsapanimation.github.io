cc29265d8cb087b2b3a8e1f6c7d8ef73bb458b08
apospelov13 <small99@hotbox.ru>
2018-02-14 15:12:17 +0300

    v4

--- js/gsap.js
+++ js/gsap.js
@@ -6,8 +6,8 @@ document.addEventListener("DOMContentLoaded", function(event) {
   
   let slider = [sliderItem, sliderItem2];
   
-  TweenMax.from(slider[0], 50, {x: "99%", force3D: true, rotation: 0.001, repeat: -1 });
-  TweenMax.to(slider[1], 50, {x: "-100%", force3D: true, rotation: 0.001, repeat: -1 });
+  TweenMax.from(slider[0], 150, {x: "99%", force3D: true, rotation: 0.001, repeat: -1 });
+  TweenMax.to(slider[1], 150, {x: "-100%", force3D: true, rotation: 0.001, repeat: -1 });
   // slider animation END
   
 });
